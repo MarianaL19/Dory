c2ecf1f49a9b89a2f53d02920addee3f9a576be5
Osvaldo González <gonzalezascencioosvaldo@outlook.com>
2022-05-02 21:54:22 -0500

    Agregué ventana para ver el horario y modifiqué la de agregar
    Namás creé la otra ventana y a la de agregar materia cambié los colores para que usen el tema

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import Schedule from './Windows/scheduleScreen';
 import AgendaC from './Windows/agendaScreen';
 import HorarioAdd from './Windows/horarioAddScreen';
 import HorarioEmpty from './Windows/horarioEmptyScreen';
+import HorarioShow from './Windows/horarioShowScreen';
 import Settings from './Windows/SettingsScreen';
 
 
