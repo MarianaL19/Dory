9c9031f499d9120cf8c7cbbdccda5cda38d5ec57
CesarHigashi <100962562+CesarHigashi@users.noreply.github.com>
2022-05-08 23:06:39 -0500

    Update App.js

--- App.js
+++ App.js
@@ -5,11 +5,20 @@ import HorarioAdd from './Windows/horarioAddScreen';
 import HorarioEmpty from './Windows/horarioEmptyScreen';
 import HorarioShow from './Windows/horarioShowScreen';
 import Settings from './Windows/SettingsScreen';
+import RecordatoriosAdd from './Windows/recordatoriosAddScreen';
+import Recordatorios from './Windows/recordatoriosScreen';
 
 
 export default function App(){
 
   return (
+    //<Schedule/>
     <AgendaC/>
+    //<HorarioAdd/>
+    //<HorarioEmpty/>
+    //<HorarioShow/>
+    //<Settings/>
+    //<RecordatoriosAdd/>
+    //<Recordatorios/>    
   );
 }
