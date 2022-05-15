72275615134d0eff44b447e4d8e3a504d26ca178
MarianaL19 <joscelynn7u7@gmail.com>
2022-05-15 00:45:34 -0500

    update: Conexión BD formulario recordatorio
    Ya está conectado el formulario de recordatorios con la BD.

--- Windows/recordatoriosAddScreen.js
+++ Windows/recordatoriosAddScreen.js
@@ -12,160 +12,184 @@ export default class RecordatoriosAddScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      tag: 'null',
+      nombre: '',
+      etiqueta: 'null',
+      materia: 0,
       fecha: new Date(),
       fechaOpen: false,
       textFecha: 'Selecciona la fecha de entrega',
       hora: new Date(),
       horaOpen: false,
       textHora: 'Selecciona la hora de entrega',
-  };
+      descripcion: '',
+      estado: 'pendiente',
+    };
 
-}
+  }
 
-render() {
-  return (
-    <View style={styles.container}>
-    <ScrollView>
-      <Text style={[styles.title, { color: currentTheme.quaternaryColor }]}>Nuevo recordatorio</Text>
+  render() {
 
-      <View style={{ borderBottomColor: currentTheme.quinaryColor, borderBottomWidth: 1, width: width }} />
+    const registro = () => {
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
 
-      <TextInput placeholder="Título del recordatorio" keyboardType="default" style={[styles.inputs, { marginVertical: 5, paddingHorizontal: 10 }]} />
+        }
+      };
+      xhttp.open("GET", 'https://dory69420.000webhostapp.com/recordatorios.php?nombre=' + this.state.nombre
+        + '&etiqueta=' + this.state.etiqueta + '&materia=' + this.state.materia + '&fecha=' + this.state.textFecha
+        + '&hora=' + this.state.textHora + '&descripcion=' + this.state.descripcion + '&estado=' + this.state.estado, true);
+      xhttp.send();
 
-      <View style={{ borderBottomColor: currentTheme.quinaryColor, borderBottomWidth: 1, width: width }} />
+      console.log('nombre: '+ this.state.nombre + '  etiqueta: '+ this.state.etiqueta + '  materia: '+ this.state.materia + '  estado: ' + this.state.estado +
+      '  fecha: ' + this.state.textFecha + '  hora: ' + this.state.textHora)
+    }
 
-      {/* ~~~~~~~~ Etiquetas de tipo de recordatorio ~~~~~~~~ */}
-      <Text style={[styles.subtitle, { color: currentTheme.tertiaryColor }]}>Tipo de recordatorio</Text>
+    return (
+      <View style={styles.container}>
+        <ScrollView>
+          <Text style={[styles.title, { color: currentTheme.quaternaryColor }]}>Nuevo recordatorio</Text>
 
-      <View style={styles.filterButtonContainer}>
-        <TouchableOpacity onPress={() => this.setState({tag: 'tarea'})}
-          style={[styles.filterButton, this.state.tag == 'tarea' ? { backgroundColor: currentTheme.quinaryColor } : {}]}>
-          <Text style={[styles.filterText, this.state.tag == 'tarea' ? { color: currentTheme.primaryColor } : {}]}>Tarea</Text>
-        </TouchableOpacity>
+          <View style={{ borderBottomColor: currentTheme.quinaryColor, borderBottomWidth: 1, width: width }} />
 
-        <TouchableOpacity onPress={() => this.setState({tag: 'examen'})}
-          style={[styles.filterButton, this.state.tag == 'examen' ? { backgroundColor: currentTheme.quinaryColor } : {}]}>
-          <Text style={[styles.filterText, this.state.tag == 'examen' ? { color: currentTheme.primaryColor } : {}]}>Examen</Text>
-        </TouchableOpacity>
+          <TextInput placeholder="Título del recordatorio" keyboardType="default" style={[styles.inputs, { marginVertical: 5, paddingHorizontal: 10 }]}
+            onChangeText={(value) => this.setState({ nombre: value })} />
 
-        <TouchableOpacity onPress={() => this.setState({tag: 'otro'})}
-          style={[styles.filterButton, this.state.tag == 'otro' ? { backgroundColor: currentTheme.quinaryColor } : {}]}>
-          <Text style={[styles.filterText, this.state.tag == 'otro' ? { color: currentTheme.primaryColor } : {}]}>Otro</Text>
-        </TouchableOpacity>
-      </View>
+          <View style={{ borderBottomColor: currentTheme.quinaryColor, borderBottomWidth: 1, width: width }} />
 
-      <View style={{ borderBottomColor: currentTheme.quinaryColor, borderBottomWidth: 1, width: width }} />
+          {/* ~~~~~~~~ Etiquetas de tipo de recordatorio ~~~~~~~~ */}
+          <Text style={[styles.subtitle, { color: currentTheme.tertiaryColor }]}>Tipo de recordatorio</Text>
+
+          <View style={styles.filterButtonContainer}>
+            <TouchableOpacity onPress={() => this.setState({ etiqueta: 'tarea' })}
+              style={[styles.filterButton, this.state.etiqueta == 'tarea' ? { backgroundColor: currentTheme.quinaryColor } : {}]}>
+              <Text style={[styles.filterText, this.state.etiqueta == 'tarea' ? { color: currentTheme.primaryColor } : {}]}>Tarea</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={() => this.setState({ etiqueta: 'examen' })}
+              style={[styles.filterButton, this.state.etiqueta == 'examen' ? { backgroundColor: currentTheme.quinaryColor } : {}]}>
+              <Text style={[styles.filterText, this.state.etiqueta == 'examen' ? { color: currentTheme.primaryColor } : {}]}>Examen</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={() => this.setState({ etiqueta: 'otro' })}
+              style={[styles.filterButton, this.state.etiqueta == 'otro' ? { backgroundColor: currentTheme.quinaryColor } : {}]}>
+              <Text style={[styles.filterText, this.state.etiqueta == 'otro' ? { color: currentTheme.primaryColor } : {}]}>Otro</Text>
+            </TouchableOpacity>
+          </View>
+
+          <View style={{ borderBottomColor: currentTheme.quinaryColor, borderBottomWidth: 1, width: width }} />
 
 
-      {/* ~~~~~~~~ Materia ~~~~~~~~ */}
+          {/* ~~~~~~~~ Materia ~~~~~~~~ */}
 
-      {this.state.tag != 'otro' ? (
-        <>
+          {this.state.etiqueta != 'otro' ? (
+            <>
+              <View style={styles.iconContainer}>
+                <Icon name='book-open-page-variant' size={25} color='#A9A9A9' />
+                <TextInput placeholder="Materia" keyboardType="default" style={styles.inputs}
+                  onChangeText={(value) => this.setState({ materia: value })} />
+              </View>
+              <View style={{ borderBottomColor: currentTheme.quinaryColor, borderBottomWidth: 1, width: width }} />
+            </>
+          ) : null}
+
+
+          {/* ~~~~~~~~ Seleccionar la fecha y hora de entrega ~~~~~~~~ */}
+
           <View style={styles.iconContainer}>
-            <Icon name='book-open-page-variant' size={25} color='#A9A9A9' />
-            <TextInput placeholder="Materia" keyboardType="default" style={styles.inputs} />
+            <Icon name='calendar-blank-outline' size={25} color='#A9A9A9' />
+            <TouchableOpacity onPress={() => this.setState({ fechaOpen: true })}>
+              <Text style={styles.inputs}>{cambioFormato(this.state.textFecha)}</Text>
+            </TouchableOpacity>
+          </View>
+
+          <View style={styles.iconContainer}>
+            <Icon name='clock-time-four-outline' size={25} color='#A9A9A9' />
+            <TouchableOpacity onPress={() => this.setState({ horaOpen: true })}>
+              <Text style={styles.inputs}>{this.state.textHora}</Text>
+            </TouchableOpacity>
+          </View>
+
+          <View style={{ borderBottomColor: currentTheme.quinaryColor, borderBottomWidth: 1, width: width }} />
+
+          {/* ~~~~~~~~ Descripción ~~~~~~~~ */}
+
+          <View style={styles.iconContainer}>
+            <Icon name='pencil' size={25} color='#A9A9A9' />
+            <TextInput multiline={true} numberOfLines={3} placeholder="Descripción" keyboardType="default" style={[styles.inputs, { width: width * 0.8 }]}
+              onChangeText={(value) => this.setState({ descripcion: value })} />
           </View>
           <View style={{ borderBottomColor: currentTheme.quinaryColor, borderBottomWidth: 1, width: width }} />
-        </>
-      ) : null}
 
 
-      {/* ~~~~~~~~ Seleccionar la fecha y hora de entrega ~~~~~~~~ */}
+          {/* ~~~~~~~~ Notificar recordatorio ~~~~~~~~ */}
 
-      <View style={styles.iconContainer}>
-        <Icon name='calendar-blank-outline' size={25} color='#A9A9A9' />
-        <TouchableOpacity onPress={() => this.setState({fechaOpen: true})}> 
-          <Text style={styles.inputs}>{cambioFormato(this.state.textFecha)}</Text>
-        </TouchableOpacity>
+          <View style={[styles.iconContainer, { paddingTop: 10 }]}>
+            <Icon name='bell' size={27} color={currentTheme.primaryColor} />
+            <Text style={[styles.notificarText, { color: currentTheme.tertiaryColor }]}>Notificar recordatorio</Text>
+          </View>
+
+
+          {/* ~~~~~~~~ Opciones de notificar recordatorio ~~~~~~~~ */}
+
+          <View style={styles.opcRecordatorioContainer}>
+            <Text style={[styles.recordatoriosText, { color: currentTheme.tertiaryColor }]}>24 horas antes</Text>
+            <Switch trackColor={{ false: "grey", true: "grey" }} />
+          </View>
+
+          <View style={styles.opcRecordatorioContainer}>
+            <Text style={[styles.recordatoriosText, { color: currentTheme.tertiaryColor }]}>12 horas antes</Text>
+            <Switch trackColor={{ false: "grey", true: "grey" }} />
+          </View>
+
+          <View style={styles.opcRecordatorioContainer}>
+            <Text style={[styles.recordatoriosText, { color: currentTheme.tertiaryColor }]}>Seleccionar hora</Text>
+          </View>
+
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity onPress={registro} style={[styles.addButton, { backgroundColor: currentTheme.primaryColor }]}>
+              <Text style={styles.buttonText}>GUARDAR</Text>
+            </TouchableOpacity>
+          </View>
+
+          {/* ~~~~~~~~ Dates pickers ~~~~~~~~ */}
+          <DatePicker
+            modal
+            open={this.state.fechaOpen}
+            date={this.state.fecha}
+            locale={'es'}
+            mode={'date'}
+            onConfirm={(date) => {
+              this.setState({ fechaOpen: false })
+              this.setState({ fecha: date })
+              this.setState({ textFecha: date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate() })
+            }}
+            onCancel={() => {
+              this.setState({ fechaOpen: false })
+            }}
+          />
+
+
+          <DatePicker
+            modal
+            open={this.state.horaOpen}
+            date={this.state.hora}
+            mode={'time'}
+            onConfirm={(date) => {
+              this.setState({ horaOpen: false })
+              this.setState({ hora: date })
+              this.setState({ textHora: date.getHours() + ':' + (date.getMinutes() < 10 ? ('0' + date.getMinutes()) : date.getMinutes()) })
+            }}
+            onCancel={() => {
+              this.setState({ horaOpen: false })
+            }}
+          />
+
+        </ScrollView>
+
       </View>
-
-      <View style={styles.iconContainer}>
-        <Icon name='clock-time-four-outline' size={25} color='#A9A9A9' />
-        <TouchableOpacity onPress={() => this.setState({horaOpen: true})}>
-          <Text style={styles.inputs}>{this.state.textHora}</Text>
-        </TouchableOpacity>
-      </View>
-
-      <View style={{ borderBottomColor: currentTheme.quinaryColor, borderBottomWidth: 1, width: width }} />
-
-      {/* ~~~~~~~~ Descripción ~~~~~~~~ */}
-
-      <View style={styles.iconContainer}>
-        <Icon name='pencil' size={25} color='#A9A9A9' />
-        <TextInput multiline={true} numberOfLines={3} placeholder="Descripción" keyboardType="default" style={[styles.inputs, { width: width * 0.8 }]} />
-      </View>
-      <View style={{ borderBottomColor: currentTheme.quinaryColor, borderBottomWidth: 1, width: width }} />
-
-
-      {/* ~~~~~~~~ Notificar recordatorio ~~~~~~~~ */}
-
-      <View style={[styles.iconContainer, { paddingTop: 10 }]}>
-        <Icon name='bell' size={27} color={currentTheme.primaryColor} />
-        <Text style={[styles.notificarText, { color: currentTheme.tertiaryColor }]}>Notificar recordatorio</Text>
-      </View>
-
-
-      {/* ~~~~~~~~ Opciones de notificar recordatorio ~~~~~~~~ */}
-
-      <View style={styles.opcRecordatorioContainer}>
-        <Text style={[styles.recordatoriosText, { color: currentTheme.tertiaryColor }]}>24 horas antes</Text>
-        <Switch trackColor={{ false: "grey", true: "grey" }} />
-      </View>
-
-      <View style={styles.opcRecordatorioContainer}>
-        <Text style={[styles.recordatoriosText, { color: currentTheme.tertiaryColor }]}>12 horas antes</Text>
-        <Switch trackColor={{ false: "grey", true: "grey" }} />
-      </View>
-
-      <View style={styles.opcRecordatorioContainer}>
-        <Text style={[styles.recordatoriosText, { color: currentTheme.tertiaryColor }]}>Seleccionar hora</Text>
-      </View>
-
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity style={[styles.addButton, { backgroundColor: currentTheme.primaryColor }]}>
-          <Text style={styles.buttonText}>GUARDAR</Text>
-        </TouchableOpacity>
-      </View>
-
-      {/* ~~~~~~~~ Dates pickers ~~~~~~~~ */}
-      <DatePicker
-        modal
-        open={this.state.fechaOpen}
-        date={this.state.fecha}
-        locale={'es'}
-        mode={'date'}
-        onConfirm={(date) => {
-          this.setState({fechaOpen: false})
-          this.setState({fecha: date})
-          this.setState({textFecha: date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate()})
-        }}
-        onCancel={() => {
-          this.setState({fechaOpen: false})
-        }}
-      />
-
-
-      <DatePicker
-        modal
-        open={this.state.horaOpen}
-        date={this.state.hora}
-        mode={'time'}
-        onConfirm={(date) => {
-          this.setState({horaOpen: false})
-          this.setState({hora: date})
-          this.setState({textHora: date.getHours() + ':' + (date.getMinutes() < 10 ? ('0' + date.getMinutes()) : date.getMinutes())})
-        }}
-        onCancel={() => {
-          this.setState({horaOpen: false})
-        }}
-      />
-
-    </ScrollView>
-
-  </View>
-  );
-}
+    );
+  }
 }
 
 
