e7ffc5424ba252173858aefa71a3181fb8a13c23
MarianaL19 <joscelynn7u7@gmail.com>
2022-05-22 09:34:52 -0500

    update: filtrar recordatorios
    Los recordatorios que tienen una fecha y hora menor al día actual se van en automático a omitidos, iba a hacer un .php que cambiara también el estado en la BD pero pues alch nomás se utiliza para filtrar, no es necesario cambiarlo allá (solo es necesario cuando se completa para el checkbox, pero eso ya está hecho).

--- Windows/recordatoriosScreen.js
+++ Windows/recordatoriosScreen.js
@@ -1,16 +1,18 @@
 import React, { Component, useState } from 'react';
-import { StyleSheet, View, Text, TouchableOpacity,
-        Dimensions, FlatList, ScrollView} from 'react-native';
+import {
+  StyleSheet, View, Text, TouchableOpacity,
+  Dimensions, FlatList, ScrollView
+} from 'react-native';
 import { Collapse, CollapseHeader, CollapseBody } from "accordion-collapse-react-native";
 import { NavigationContext } from '@react-navigation/native';
-import Icon  from 'react-native-vector-icons/MaterialCommunityIcons';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 
 import MenuBar from '../hotBar';
 import currentTheme from '../Components/currentTheme';
 import RenderRecordatorio from '../Components/renderRecordatorio';
 
-const {width, height} = Dimensions.get('screen');
+const { width, height } = Dimensions.get('screen');
 
 export default class Recordatorios extends Component {
 
@@ -24,6 +26,7 @@ export default class Recordatorios extends Component {
       isOpenOmitidos: false,
       isOpenPendientes: false,
       filtroMostrar: 'Todos',
+      fechaActual: '',
     };
   }
 
@@ -31,59 +34,99 @@ export default class Recordatorios extends Component {
     var xhttp = new XMLHttpRequest();
     let _this = this;       // Esto es para usar 'this' dentro de la función
 
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-          let nombreRecordatorio = '';
-          let etiquetaRecordatorio = '';
-          let materiaRecordatorio = '';
-          let estadoRecordatorio = '';
-          let fechaRecordatorio = '';
-          let horaRecordatorio = '';
-          let descripcionRecordatorio = '';
-          let idRecordatorio = -1;
-          let checkRecordatorio = -1;
-          let marcado;
-        
-          var recordatorio = xhttp.responseText;
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        let nombreRecordatorio = '';
+        let etiquetaRecordatorio = '';
+        let materiaRecordatorio = '';
+        let estadoRecordatorio = '';
+        let fechaRecordatorio = '';
+        let horaRecordatorio = '';
+        let descripcionRecordatorio = '';
+        let idRecordatorio = -1;
+        let checkRecordatorio = -1;
+        let marcado;
+
+        var recordatorio = xhttp.responseText;
+
+        var registros = recordatorio.split('|');
+
+        var numeroRegistros = registros[0];
+
+        for (let i = 1; i <= numeroRegistros; i++) {
+          var datos = registros[i].split('¬');
+          // console.log('datos: ' + datos[0]);
+          nombreRecordatorio = datos[0];
+          etiquetaRecordatorio = datos[1];
+          materiaRecordatorio = datos[2];
+          estadoRecordatorio = datos[3];
+          fechaRecordatorio = datos[4];
+          horaRecordatorio = datos[5];
+          descripcionRecordatorio = datos[6];
+          idRecordatorio = datos[7];
+          checkRecordatorio = datos[8];
+          { checkRecordatorio == 0 ? marcado = false : marcado = true };
           
-          var registros = recordatorio.split('|');
 
-          var numeroRegistros = registros[0];
+          let fechaR = new Date(fechaRecordatorio);
+          let horaR = new Date('2020-01-01');
+          horaR.setHours(horaRecordatorio.substr(0,2));
+          horaR.setMinutes(horaRecordatorio.substr(3,2));
+          // console.log(fechaR);
 
-          for (let i=1; i<=numeroRegistros; i++){
-            var datos = registros[i].split('¬');
-            // console.log('datos: ' + datos[0]);
-            nombreRecordatorio = datos[0];
-            etiquetaRecordatorio = datos[1];
-            materiaRecordatorio = datos[2];
-            estadoRecordatorio = datos[3];
-            fechaRecordatorio = datos[4];
-            horaRecordatorio = datos[5];
-            descripcionRecordatorio = datos[6];
-            idRecordatorio = datos[7];
-            checkRecordatorio = datos[8];
-            {checkRecordatorio == 0 ? marcado = false : marcado = true};
+          let fechaA = new Date(_this.fechaActual());
+          let horaA = new Date('2020-01-01');
+          horaA.setHours(_this.horaActual().substr(0,2));
+          horaA.setMinutes(_this.horaActual().substr(3,2));
+          // console.log('actual: ' + horaA);
+          // console.log('record: ' + horaR);
 
+          // console.log('actual: ' + fechaA);
+          // console.log('record: ' + fechaR);
 
-            const objetoRecordatorio =
-            {nombre: nombreRecordatorio, etiqueta: etiquetaRecordatorio,
-             materia: materiaRecordatorio, estado: estadoRecordatorio,
-             fecha: fechaRecordatorio, hora: horaRecordatorio,
-             descripcion: descripcionRecordatorio, id: idRecordatorio, check: marcado};
-
-            const nuevoArreglo = [..._this.state.listaRecordatorios, objetoRecordatorio];
-            _this.setState({listaRecordatorios: nuevoArreglo});
-            // console.log(objetoRecordatorio);
+          if(fechaR < fechaA && estadoRecordatorio!='completado'){
+            estadoRecordatorio = 'omitido';
+          }else if(fechaR.getTime() === fechaA.getTime() && horaR.getTime() < horaA.getTime() && estadoRecordatorio!='completado'){
+            estadoRecordatorio = 'omitido';
           }
-          // console.log(_this.state.listaRecordatorios);
+
+          const objetoRecordatorio =
+          {
+            nombre: nombreRecordatorio, etiqueta: etiquetaRecordatorio,
+            materia: materiaRecordatorio, estado: estadoRecordatorio,
+            fecha: fechaRecordatorio, hora: horaRecordatorio,
+            descripcion: descripcionRecordatorio, id: idRecordatorio, check: marcado
+          };
+
+          const nuevoArreglo = [..._this.state.listaRecordatorios, objetoRecordatorio];
+          _this.setState({ listaRecordatorios: nuevoArreglo });
+          // console.log(objetoRecordatorio);
         }
+        // console.log(_this.state.listaRecordatorios);
+      }
     };
     xhttp.open("GET", 'https://dory69420.000webhostapp.com/recuperarRecordatorios.php'
-    , true);
+      , true);
     xhttp.send();
-    }
+  }
 
-  componentDidMount(){
+
+  fechaActual = () => {
+    let actual = new Date();
+    let fechaActual = actual.getFullYear() + '-' +  ((actual.getMonth() + 1) < 10 ? ('0' + (actual.getMonth() + 1)) : (actual.getMonth() + 1)) + '-' + actual.getDate()
+    // console.log(fechaActual);
+    return fechaActual;
+  }
+
+  horaActual = () => {
+    let actual = new Date();
+    let horaActual = (actual.getHours() < 10 ? ('0' + actual.getHours()) : actual.getHours()) + ':' + (actual.getMinutes() < 10 ? ('0' + actual.getMinutes()) : actual.getMinutes());
+    // console.log(horaActual);
+    return horaActual;
+  }
+
+
+  componentDidMount() {
     this.recuperarDatos();
   }
 
@@ -92,131 +135,131 @@ export default class Recordatorios extends Component {
 
     return (
       <>
-      <View style = {{backgroundColor: currentTheme.backgroundColor, flex: 1}}>
+        <View style={{ backgroundColor: currentTheme.backgroundColor, flex: 1 }}>
 
-        <View style={styles.nav}>
-          <MenuBar/>
-        </View>
-
-        <View style={styles.filterButtonContainer}>
-          
-          <TouchableOpacity
-          onPress={() => this.setState({ filtroMostrar: 'Todos'})}
-          style={[styles.filterButton, this.state.filtroMostrar == 'Todos' ? { backgroundColor: currentTheme.quinaryColor } : {} ]}
-          >
-            <Text
-            style={[styles.filterText, this.state.filtroMostrar == 'Todos' ? { color: currentTheme.primaryColor } : {}]}
-            onPress={() => {this.state.recordatoriosPendientes = this.state.recordatoriosPendientes.slice(0, 1); console.log(this.state.listaRecordatorios)}}
-            >
-              Todos
-            </Text>
-          </TouchableOpacity>
-          
-          <TouchableOpacity
-          onPress={() => this.setState({ filtroMostrar: 'Tareas'})}
-          style={[styles.filterButton, this.state.filtroMostrar == 'Tareas' ? { backgroundColor: currentTheme.quinaryColor } : {} ]}
-          >
-            <Text
-            style={[styles.filterText, this.state.filtroMostrar == 'Tareas' ? { color: currentTheme.primaryColor } : {}]}
-            >
-              Tareas
-            </Text>
-          </TouchableOpacity>
-          
-          <TouchableOpacity
-          onPress={() => this.setState({ filtroMostrar: 'Exámenes'})}
-          style={[styles.filterButton, this.state.filtroMostrar == 'Exámenes' ? { backgroundColor: currentTheme.quinaryColor } : {} ]}
-          >
-            <Text
-            style={[styles.filterText, this.state.filtroMostrar == 'Exámenes' ? { color: currentTheme.primaryColor } : {}]}
-            >
-              Exámenes
-            </Text>
-          </TouchableOpacity>
-        
-          <TouchableOpacity
-          onPress={() => this.setState({ filtroMostrar: 'Otros'})}
-          style={[styles.filterButton, this.state.filtroMostrar == 'Otros' ? { backgroundColor: currentTheme.quinaryColor } : {} ]}
-          >
-            <Text
-            style={[styles.filterText, this.state.filtroMostrar == 'Otros' ? { color: currentTheme.primaryColor } : {}]}
-            >
-              Otros
-            </Text>
-          </TouchableOpacity>
-        </View>
-
-        <ScrollView>
-          <View style = {{marginBottom: '10%'}}>
-            <Collapse
-              onToggle={() => this.setState({ isOpenCompletados: !this.state.isOpenCompletados })}
-            >
-              <CollapseHeader>
-                <View style = {{flexDirection: 'row'}}>
-                  <Text style={styles.collapseTitle}>Completados</Text>
-                  <Icon name={this.state.isOpenCompletados === true ? 'chevron-up' : 'chevron-down'} size={30} color={currentTheme.primaryColor}/>
-                </View>
-              </CollapseHeader>
-              <CollapseBody>
-              <FlatList
-                    data = {this.state.listaRecordatorios.filter(objetoRecordatorio => objetoRecordatorio.estado === 'completado')}
-
-                    renderItem={({item}) => <RenderRecordatorio item={item} />}
-                />
-              </CollapseBody>
-            </Collapse>
-
-            <Collapse
-              onToggle={() => this.setState({ isOpenOmitidos: !this.state.isOpenOmitidos })}
-            >
-              <CollapseHeader>
-                <View style = {{flexDirection: 'row'}}>
-                  <Text style={styles.collapseTitle}>Omitidos</Text>
-                  <Icon name={this.state.isOpenOmitidos === true ? 'chevron-up' : 'chevron-down'} size={30} color={currentTheme.primaryColor}/>
-                </View>
-              </CollapseHeader>
-              <CollapseBody>
-              <FlatList
-                    data = {this.state.listaRecordatorios.filter(objetoRecordatorio => objetoRecordatorio.estado === 'omitido')}
-
-                    renderItem={({item}) => <RenderRecordatorio item={item} />}
-                />
-              </CollapseBody>
-            </Collapse>
-
-            <Collapse
-              onToggle={() => this.setState({ isOpenPendientes: !this.state.isOpenPendientes })}
-            >
-              <CollapseHeader>
-                <View style = {{flexDirection: 'row'}}>
-                  <Text style={styles.collapseTitle}>Pendientes</Text>
-                  <Icon name={this.state.isOpenPendientes === true ? 'chevron-up' : 'chevron-down'} size={30} color={currentTheme.primaryColor}/>
-                </View>
-              </CollapseHeader>
-              <CollapseBody>
-                <FlatList
-                    data = {this.state.listaRecordatorios.filter(objetoRecordatorio => objetoRecordatorio.estado === 'pendiente')}
-
-                    renderItem={({item}) => <RenderRecordatorio item={item} />}
-                />
-              </CollapseBody>
-            </Collapse>
+          <View style={styles.nav}>
+            <MenuBar />
           </View>
-        </ScrollView>
 
-      </View>
+          <View style={styles.filterButtonContainer}>
 
-      {/* Botón para agregar recordatorios */}
-      <TouchableOpacity onPress={() => {navigation.navigate("AddRecordatorio");}} style={styles.addIcon}>
-        <Icon name='plus-circle' size={50} color={currentTheme.primaryColor}/>
-      </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => this.setState({ filtroMostrar: 'Todos' })}
+              style={[styles.filterButton, this.state.filtroMostrar == 'Todos' ? { backgroundColor: currentTheme.quinaryColor } : {}]}
+            >
+              <Text
+                style={[styles.filterText, this.state.filtroMostrar == 'Todos' ? { color: currentTheme.primaryColor } : {}]}
+                onPress={() => { this.state.recordatoriosPendientes = this.state.recordatoriosPendientes.slice(0, 1); console.log(this.state.listaRecordatorios) }}
+              >
+                Todos
+              </Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              onPress={() => this.setState({ filtroMostrar: 'Tareas' })}
+              style={[styles.filterButton, this.state.filtroMostrar == 'Tareas' ? { backgroundColor: currentTheme.quinaryColor } : {}]}
+            >
+              <Text
+                style={[styles.filterText, this.state.filtroMostrar == 'Tareas' ? { color: currentTheme.primaryColor } : {}]}
+              >
+                Tareas
+              </Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              onPress={() => this.setState({ filtroMostrar: 'Exámenes' })}
+              style={[styles.filterButton, this.state.filtroMostrar == 'Exámenes' ? { backgroundColor: currentTheme.quinaryColor } : {}]}
+            >
+              <Text
+                style={[styles.filterText, this.state.filtroMostrar == 'Exámenes' ? { color: currentTheme.primaryColor } : {}]}
+              >
+                Exámenes
+              </Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              onPress={() => this.setState({ filtroMostrar: 'Otros' })}
+              style={[styles.filterButton, this.state.filtroMostrar == 'Otros' ? { backgroundColor: currentTheme.quinaryColor } : {}]}
+            >
+              <Text
+                style={[styles.filterText, this.state.filtroMostrar == 'Otros' ? { color: currentTheme.primaryColor } : {}]}
+              >
+                Otros
+              </Text>
+            </TouchableOpacity>
+          </View>
+
+          <ScrollView>
+            <View style={{ marginBottom: '10%' }}>
+              <Collapse
+                onToggle={() => this.setState({ isOpenCompletados: !this.state.isOpenCompletados })}
+              >
+                <CollapseHeader>
+                  <View style={{ flexDirection: 'row' }}>
+                    <Text style={styles.collapseTitle}>Completados</Text>
+                    <Icon name={this.state.isOpenCompletados === true ? 'chevron-up' : 'chevron-down'} size={30} color={currentTheme.primaryColor} />
+                  </View>
+                </CollapseHeader>
+                <CollapseBody>
+                  <FlatList
+                    data={this.state.listaRecordatorios.filter(objetoRecordatorio => objetoRecordatorio.estado === 'completado')}
+
+                    renderItem={({ item }) => <RenderRecordatorio item={item} />}
+                  />
+                </CollapseBody>
+              </Collapse>
+
+              <Collapse
+                onToggle={() => this.setState({ isOpenOmitidos: !this.state.isOpenOmitidos })}
+              >
+                <CollapseHeader>
+                  <View style={{ flexDirection: 'row' }}>
+                    <Text style={styles.collapseTitle}>Omitidos</Text>
+                    <Icon name={this.state.isOpenOmitidos === true ? 'chevron-up' : 'chevron-down'} size={30} color={currentTheme.primaryColor} />
+                  </View>
+                </CollapseHeader>
+                <CollapseBody>
+                  <FlatList
+                    data={this.state.listaRecordatorios.filter(objetoRecordatorio => objetoRecordatorio.estado === 'omitido')}
+
+                    renderItem={({ item }) => <RenderRecordatorio item={item} />}
+                  />
+                </CollapseBody>
+              </Collapse>
+
+              <Collapse
+                onToggle={() => this.setState({ isOpenPendientes: !this.state.isOpenPendientes })}
+              >
+                <CollapseHeader>
+                  <View style={{ flexDirection: 'row' }}>
+                    <Text style={styles.collapseTitle}>Pendientes</Text>
+                    <Icon name={this.state.isOpenPendientes === true ? 'chevron-up' : 'chevron-down'} size={30} color={currentTheme.primaryColor} />
+                  </View>
+                </CollapseHeader>
+                <CollapseBody>
+                  <FlatList
+                    data={this.state.listaRecordatorios.filter(objetoRecordatorio => objetoRecordatorio.estado === 'pendiente')}
+
+                    renderItem={({ item }) => <RenderRecordatorio item={item} />}
+                  />
+                </CollapseBody>
+              </Collapse>
+            </View>
+          </ScrollView>
+
+        </View>
+
+        {/* Botón para agregar recordatorios */}
+        <TouchableOpacity onPress={() => { navigation.navigate("AddRecordatorio"); }} style={styles.addIcon}>
+          <Icon name='plus-circle' size={50} color={currentTheme.primaryColor} />
+        </TouchableOpacity>
       </>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  nav:{
+  nav: {
     width: width,
     height: 60,
   },
